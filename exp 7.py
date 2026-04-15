0b3bcb4872859dcb03c6a7c5c5402ad18e10d650
tasmiya khanum <juug24bca38655@jainuniversity.ac.in>
2026-04-15 22:35:36 +0530

    Add OOP concepts demonstration in exp 7.py
    Add a description for demonstrating OOP concepts in Python.

---
+++ exp 7.py
@@ -0,0 +1,2 @@
+Demonstrate use of object- oriented programming concepts
+To study and demonstrate the use of Object-Oriented Programming (OOP) concepts such as class, object, encapsulation, inheritance, polymorphism, and abstraction using Python.
